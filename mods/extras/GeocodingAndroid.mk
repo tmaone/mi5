2da53a641f088edd04519f51bdeed5b29684e2f9
tmaone <marios.daoutis@gmail.com>
2016-08-20 19:52:48 +0200

    fx

--- mods/extras/GeocodingAndroid.mk
+++ mods/extras/GeocodingAndroid.mk
@@ -7,16 +7,15 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := NominatimGeocoderBackend
 
 geoco_root  := $(LOCAL_PATH)
-geoco_dir   := app
 geoco_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
 geoco_build := $(geoco_root)/$(geoco_dir)/build
 geoco_apk   := build/outputs/apk/$(geoco_dir)-release-unsigned.apk
 
-$(geoco_root)/$(geoco_dir)/$(geoco_apk):
+$(geoco_root)/$(geoco_apk):
 	rm -Rf $(geoco_build)
 	mkdir -p $(geoco_out)
 	ln -sf $(geoco_out) $(geoco_build)
-	cd $(geoco_root)/$(geoco_dir) && gradle assembleRelease
+	cd $(geoco_root) && gradle assembleRelease
 
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := $(geoco_dir)/$(geoco_apk)
